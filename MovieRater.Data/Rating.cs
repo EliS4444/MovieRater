5e993cfabecbd08a292e674b0afca7a1c128e8f7
Patom Thirawetchayan <patomt@gmail.com>
2022-01-20 10:55:20 -0500

    create rating object

---
+++ MovieRater.Data/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Data
+{
+    public class Rating
+    {
+        [Key]
+        public int RatingId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public double Score { get; set; }
+        [Required]
+        [Display(Name ="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
