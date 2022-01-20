0ec0951bf0d9e3736fbf83b5938ddc2bffc062f1
Patom Thirawetchayan <patomt@gmail.com>
2022-01-20 12:33:27 -0500

    added createRatingMethod

--- MovieRater.Models/RatingCreate.cs
+++ MovieRater.Models/RatingCreate.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieRater.Models
 {
-    internal class RatingCreate
+    public class RatingCreate
     {
+        [Required]
+        [Range(0,5)]
+        public double Score { get; set; }
+        [MaxLength(255)]
+        public string Remarks { get; set; }
     }
 }
--- MovieRater.Services/RatingService.cs
+++ MovieRater.Services/RatingService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieRater.Data;
+using MovieRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace MovieRater.Services
 {
-    internal class RatingService
+    public class RatingService
     {
+        private readonly Guid _userId;
+
+        public RatingService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateRating(RatingCreate model)
+        {
+            var entity =
+                new Rating()
+                {
+                    OwnerId = _userId,
+                    Score = model.Score,
+                    Remarks = model.Remarks,
+                    CreatedUtc = DateTimeOffset.Now,
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Ratings.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
