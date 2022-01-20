5f73209b8565fb5f5a85d42a5cdd4cdc5d410e48
Patom Thirawetchayan <patomt@gmail.com>
2022-01-20 12:25:56 -0500

    reconciliation commit

---
+++ MovieRater.Services/RatingService.cs
@@ -0,0 +1,38 @@
+﻿using MovieRater.Data;
+using MovieRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Services
+{
+    public class RatingService
+    {
+        private readonly Guid _userId;
+
+        public RatingService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateRating(RatingCreate model)
+        {
+            var entity =
+                new Rating()
+                {
+                    OwnerId = _userId,
+                    Score = model.Score,
+                    Remarks = model.Remarks,
+                    CreatedUtc = DateTimeOffset.Now,
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Ratings.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
